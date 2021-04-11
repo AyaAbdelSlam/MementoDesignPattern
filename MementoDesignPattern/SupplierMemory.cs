dd1769588315b4a3a1e6ab16c27913182bb7274c
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2021-04-11 16:08:32 +0200

    Initial Create

---
+++ MementoDesignPattern/SupplierMemory.cs
@@ -0,0 +1,18 @@
+﻿namespace MementoDesignPattern
+{
+    /// <summary>
+    /// The Caretaker class.  
+    /// This class never examines the contents of any Memento and is
+    /// responsible for keeping that memento.
+    /// </summary>
+    internal class SupplierMemory
+    {
+        private FoodSupplierMemento _memento;
+
+        public FoodSupplierMemento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
---
+++ OtherExample/ProspectMemory.cs
@@ -0,0 +1,21 @@
+﻿namespace OtherExample
+{
+    /// <summary>
+
+    /// The 'Caretaker' class
+
+    /// </summary>
+    internal class ProspectMemory
+
+    {
+        private Memento _memento;
+
+        // Property
+
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
